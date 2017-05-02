234818c09f4a502108fbe05d3dba791cc06bc48d
Julie S. Mike <juliesmike@gmail.com>
2017-05-02 16:02:11 -0500

    loop for posts, different layout pages, formating

--- wp-content/themes/wpsample/footer.php
+++ wp-content/themes/wpsample/footer.php
@@ -1,7 +1,7 @@
 </div><!-- /.container -->
 
 <footer class="blog-footer">
-  <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+  <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/motbsitu" target="_blank">@motbsitu</a>.</p>
   <p>
     <a href="#">Back to top</a>
   </p>
