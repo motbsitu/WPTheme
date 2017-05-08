a2db5da2bc42f1874e5fc0eb0ea1b6680321921e
Julie S. Mike <juliesmike@gmail.com>
2017-05-08 10:06:47 -0500

    theme dev start

--- wp-content/plugins/wp-cbf/admin/class-wp-cbf-admin.php
+++ wp-content/plugins/wp-cbf/admin/class-wp-cbf-admin.php
@@ -110,8 +110,12 @@ public function add_plugin_admin_menu(){
 //Settings action link
 public function add_action_links( $links ){
 	$settings_link = array(
-		'<a aref="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+		'<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
    );
 	 return array_merge( $settings_link, $links );
+}
+
+public function display_plugin_setup_page(){
+	include_once( 'partials/wp-cbf-admin-display.php');
 }
 }
